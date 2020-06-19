13cc340cccf0c39c6c1c4dbd5083737edb5919d4
Andrew Royce <andrew.w.royce@gmail.com>
2020-06-19 14:03:33 -0400

    updated prop style

--- src/components/text-button/index.js
+++ src/components/text-button/index.js
@@ -21,7 +21,7 @@ export default class TextButton extends PureComponent {
 
     title: PropTypes.string.isRequired,
     titleColor: PropTypes.string,
-    titleStyle: Animated.Text.propTypes.style,
+    titleStyle: PropTypes.style,
     disabledTitleColor: PropTypes.string,
   };
 
